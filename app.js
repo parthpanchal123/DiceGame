e2c4694d178f91571efc8a8cf272206242631d5b
anukindipa <61876990+anukindipa@users.noreply.github.com>
2021-10-08 14:15:34 +0530

    added small shaking animation to the dice
    made the dice shake a little before the next one comes up.

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ function gen(){
     var dom_score = document.getElementById("score");
  
     var num = Math.floor(Math.random()*6)+1 ;
+    
+    //adds shaking attribute to the dice
+    img_dom.classList.add("shake");
+    
+    setTimeout(function(){
+    //stops the shaking and changes the dice value after 500 ms
+    
+    img_dom.classList.remove("shake")
 
     img_dom.src="dice-"+num+".png" ;
 
@@ -16,7 +24,7 @@ function gen(){
 
     dom_score.textContent = "Total score : " + (score) ;
 
-    
+    }, 500);
     
 }
 
